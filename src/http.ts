39ee0f206e78f1e136a6234c21f0f16910851b89
Bernardo Pereira Oliveira <bernardo.258@hotmail.com>
2022-07-14 20:15:23 -0300

    upda: create base socket

--- src/http.ts
+++ src/http.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import http from "http";
-import path from "path";
 import { Server } from "socket.io";
 const app = express();
-app.use(express.static(path.join(__dirname, "..", "public")));
 const serverHTTP = http.createServer(app);
-const io = new Server(serverHTTP);
+
+const io = new Server(serverHTTP, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
 export { serverHTTP, io }
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,55 @@
 import { io } from "./http";
 
+interface RoomUser {
+    room: string;
+    username: string;
+    socketId: string;
+}
+
+interface Message {
+    room: string,
+    message: string;
+    createdAt: Date,
+    username: string;
+}
+
+const users: RoomUser[] = [];
+const messages: Message[] = [];
+
 io.on("connection", socket => {
     console.log(socket.id);
+
+    socket.on("room", data => {
+        console.log("nova usuário na sala", data);
+        socket.join(data);
+
+        const userInRoom = users.find(user => {
+            return user.username === data.username
+                   && user.room === data.room
+        })
+
+        if(userInRoom) {
+            userInRoom.socketId = socket.id;
+            return
+        }
+        
+        users.push({
+            room: data.room,
+            username: data.username,
+            socketId: data.socketId,
+        })
+    })
+
+    socket.on("send-message", data => {
+        console.log("nova mensagem", data);
+        const newMessage: Message = {
+            room: data.room,
+            username: data.username,
+            message: data.message,
+            createdAt: new Date(),
+        }
+        messages.push(newMessage);
+        socket.to(data.room).emit("receive-message", newMessage);
+    })
+
 });
